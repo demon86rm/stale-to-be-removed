3eeb9465968bc74c9c0192ee8f54c299122fa9ce
mplacidi <mplacidi@redhat.com>
2023-12-07 15:10:49 +0100

    various fixes

--- cloud_updater.sh
+++ cloud_updater.sh
@@ -16,8 +16,12 @@ fi
 MACORLINUX=$(uname -a|grep Darwin;echo $?)
 if [ "${MACORLINUX}" == 0 ];
 	then ostype=macos
+	ARCH=$(uname -m)
+	SYSTEM=macos
 else
 	ostype=linux
+	ARCH=$(uname -m)
+	SYSTEM=$(uname -s|tr '[:upper:]' '[:lower:]')
 fi
 
 # Global Vars section
@@ -28,12 +32,26 @@ fi
 
 ## check if there's any Package Manager 
 
+declare -A osInfo;
+osInfo[/etc/redhat-release]="rpm -q"
+osInfo[/etc/SuSE-release]="zypp search -i"
+osInfo[/etc/debian_version]="dpkg -l"
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        PKGMGR=${osInfo[$f]}
+    fi
+done
+
+
 
 # Client Array section
 
 declare -A CLIENT_URLS_ARRAY
 CLIENT_URLS_ARRAY[oc]="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz"
 CLIENT_URLS_ARRAY[ocm]="https://github.com/openshift-online/ocm-cli/releases/download/${OCM_VERSION}/ocm-linux-amd64"
+#CLIENT_URLS_ARRAY[tkn]="https://mirror.openshift.com/pub/openshift-v4/clients/pipelines/latest/${ARCH}/${SYSTEM}/tkn-linux-amd64.tar.gz"
 CLIENT_URLS_ARRAY[tkn]="https://mirror.openshift.com/pub/openshift-v4/clients/pipelines/latest/tkn-linux-amd64.tar.gz"
 CLIENT_URLS_ARRAY[kn]="https://mirror.openshift.com/pub/openshift-v4/clients/serverless/latest/kn-linux-amd64.tar.gz"
 CLIENT_URLS_ARRAY[rosa]="https://mirror.openshift.com/pub/openshift-v4/clients/rosa/latest/rosa-linux.tar.gz"
@@ -124,33 +142,120 @@ linux_client_check_n_update () {
      	then echo "$client already installed in your system" "now checking downloaded version vs. installed version md5 checksums"; 
      		if [ "$client" == "ocm" ];
      			then export CLIENT_MD5=$(md5sum ${TMPDIR}/${CLIENT_FILENAME} |grep -A1 $client|tail -n1|sed -E 's/\s.+$//g')
-		elif [ "$client" == "az" ];
-			PKGCHECK=$(rpm -q azure-cli > /dev/null;echo $?)
-			[[ -x $PKGMGR && $PKGMGR == *"rpm"* ]] && $PKGMGR -q azure-cli 2>/dev/null >/dev/null ;echo $? || $PKGMGR -l azure-cli 2>/dev/null^C
-
-			then echo "Check cannot be implemented for azure-cli, if you continue with the script you'll overwrite your current installation"
+	        elif [[ "$client" == "az" || "$client" == "aws" ]];
+			then echo "Check cannot be implemented for azure-cli/aws-cli, the script will invoke an installer that'll lets you install/update the binary"
      		else
      			export CLIENT_MD5=$(tar xvfz ${TMPDIR}/${CLIENT_FILENAME} --to-command=md5sum|grep -A1 $client|tail -n1|sed -E 's/\s.+$//g') 
      		fi
      	     export LOCAL_CLIENT_MD5=$(md5sum $CLIENT_CHECK|sed -E 's/\s.+$//g')
      	     if [ "${CLIENT_MD5}" == "${LOCAL_CLIENT_MD5}" ];
-     	     then echo "Already downloaded $client client with md5sum ${LOCAL_CLIENT_MD5}";
+     	     	then echo "Already downloaded $client client with md5sum ${LOCAL_CLIENT_MD5}";
+	     elif [ "$client" == "az" ];
+	     	then echo "Check cannot be implemented for azure-cli, the script will overwrite your current installation most likely"
+					if [[ ! -z "$PKGMGR" ]];
+						then $PKGMGR azure-cli && sudo dnf update azure-cli || sudo apt-get install --only-upgrade azure-cli || sudo zypper update azure-cli
+					else
+						PYTHONCMD=$(which python3)||$(which pyhton)
+							if [ -z $PYTHONCMD ];then echo "Python is required to proceed with azure-cli installation, exiting program..";exit 2;fi
+						$PYTHONCMD <(curl -s https://azurecliprod.blob.core.windows.net/install.py)
+					fi
+	     elif [[ "$client" == "aws" ]];
+	     	then curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "${TMPDIR}/awscliv2.zip" && unzip -qq -o ${TMPDIR}/awscliv2.zip -d ${TMPDIR}
+		[ -z $(which aws) ] && sudo ${TMPDIR}/aws/install || sudo ${TMPDIR}/aws/install --update
 	     else
 		     # Untar and copy/overwrite to CLIENT_LOC
-			echo "Now unTar-ing $client client into $CLIENT_LOC" 
-			if [[ "$CLIENT_FILENAME" == *".tar.gz" ]];
-				then tar xvzf ${TMPDIR}/${CLIENT_FILENAME} -C $CLIENT_LOC --overwrite && echo "$client client installed/updated in $CLIENT_LOC" || echo "Please verify if your user has sufficient permissions for writing in $CLIENT_LOC, otherwise run this script with sudo"
-			elif [ "$client" == "az" ];
-				then PYTHONCMD=$(which python3)||$(which pyhton)
-					if [ -z $PYTHONCMD ];then echo "Python is required to proceed with azure-cli installation, exiting program..";exit 2;fi
-					$PYTHONCMD <(curl -s https://azurecliprod.blob.core.windows.net/install.py)
-			elif [ "$client" == "aws" ];
-			then curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "${TMPDIR}/awscliv2.zip" && unzip -o ${TMPDIR}/awscliv2.zip -d ${TMPDIR}
-				[ -z $(which aws) ] && sudo ${TMPDIR}/aws/install || sudo ${TMPDIR}/aws/install --update
-			else
-				cp ${TMPDIR}/$CLIENT_FILENAME $CLIENT_LOC/$client
+			if [[ "$CLIENT_FILENAME" == \.gz$ && "$client" != "az" && "$client" != "aws" && -w "${CLIENT_CHECK}" ]];
+				echo "Now unTar-ing $client client into $CLIENT_LOC" 
+				then tar xvzf ${TMPDIR}/${CLIENT_FILENAME} -C $CLIENT_LOC --overwrite && echo "$client client installed/updated in $CLIENT_LOC"
+			elif [[ "${client}" == "ocm" ]];
+					then cp ${TMPDIR}/$CLIENT_FILENAME $CLIENT_LOC/$client
+			else 
+				sudo tar xvzf ${TMPDIR}/${CLIENT_FILENAME} -C $CLIENT_LOC --overwrite && echo "$client client installed/updated in $CLIENT_LOC with sudo permissions"
 	     fi
 	fi
+fi
+
+}
+
+not_linux_client_check_n_update () {
+     # Defining client url for download
+     export CLIENT_URL=${CLIENT_URLS_ARRAY[$client]}
+     # Defining client binary filename
+     export CLIENT_FILENAME=$(basename $CLIENT_URL)
+     # Client env vars definition based on $client parameter chosen by the user
+     #echo $CLIENT_URL $CLIENT_FILENAME
+
+     # Defines a working directory under /tmp and defines current path
+     export TMPDIR=/tmp/${client}_cli_update_$(date +%Y.%m.%d-%H.%M.%S)
+
+     # Creates working directory and jumps in it
+     # mkdir -p ${TMPDIR}
+
+     # Determines if $client is already installed and binary location, otherwise prompts the user to enter the destination
+     CLIENT_CHECK=$(which ${client} 2>/dev/null)
+     if [ -z ${CLIENT_CHECK} ];
+        then read -p "Please enter the full path in which you desire to install the ${client} binary: " CLIENT_CHECK
+     fi
+     CLIENT_LOC=$(echo ${CLIENT_CHECK}|sed 's/\/'''${client}'''//g')
+
+     # Determines if curl and/or wget are installed and then downloads the newest $client client
+
+     CURL_CHECK=$(curl --help 2>&1 > /dev/null && echo OK || echo NO)
+
+     if [ "$CURL_CHECK" == "OK" ];
+        then
+                export URL_TOOL="$(which curl) -sO"
+        else
+                echo "Curl is needed in order to make this script functioning properly"; exit 1
+     fi
+                echo -n "Downloading latest $client client...";
+                if [ "$client" == "ocm" ];
+                        then $URL_TOOL $CLIENT_URL -L --create-dirs --output-dir ${TMPDIR} && echo "..done."
+                else
+                        $URL_TOOL $CLIENT_URL -L --create-dirs --output-dir ${TMPDIR} && echo "..done." 
+                fi
+               if [ "$?" != "0" ];then echo "Cannot download anything, please verify your network configuration.";
+               fi
+
+     # if $client already exists, check local version vs. downloaded version, exits and cleans up in case of already downloaded latest version
+     echo "Checking if $client already installed"
+     CLIENT_EXISTS=$(which $client 2>&1 >/dev/null && echo OK || echo NO)
+
+if [ "$CLIENT_EXISTS" == "OK" ];
+        then echo "$client already installed in your system" "now checking downloaded version vs. installed version md5 checksums";
+                if [ "$client" == "ocm" ];
+                        then export CLIENT_MD5=$(md5sum ${TMPDIR}/${CLIENT_FILENAME} |grep -A1 $client|tail -n1|sed -E 's/\s.+$//g')
+                elif [[ "$client" == "az" || "$client" == "aws" ]];
+                        then echo "Check cannot be implemented for azure-cli/aws-cli, the script will invoke an installer that'll lets you install/update the binary"
+                else
+                        export CLIENT_MD5=$(tar xvfz ${TMPDIR}/${CLIENT_FILENAME} --to-command=md5sum|grep -A1 $client|tail -n1|sed -E 's/\s.+$//g')
+                fi
+        export LOCAL_CLIENT_MD5=$(md5sum $CLIENT_CHECK|sed -E 's/\s.+$//g')
+                if [ "${CLIENT_MD5}" == "${LOCAL_CLIENT_MD5}" ];
+                        then echo "Already downloaded $client client with md5sum ${LOCAL_CLIENT_MD5}";
+                elif [ "$client" == "az" ];
+                then echo "Check cannot be implemented for azure-cli, the script will overwrite your current installation most likely"
+                        if [[ ! -z "$PKGMGR" ]];
+                                then $PKGMGR azure-cli && sudo dnf update azure-cli || sudo apt-get install --only-upgrade azure-cli || sudo zypper update azure-cli
+                        else
+                                PYTHONCMD=$(which python3)||$(which pyhton)
+                                        if [ -z $PYTHONCMD ];then echo "Python is required to proceed with azure-cli installation, exiting program..";exit 2;fi
+                                                $PYTHONCMD <(curl -s https://azurecliprod.blob.core.windows.net/install.py)
+                                        fi
+                elif [[ "$client" == "aws" ]];
+                        then curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "${TMPDIR}/awscliv2.zip" && unzip -qq -o ${TMPDIR}/awscliv2.zip -d ${TMPDIR}
+                        [ -z $(which aws) ] && sudo ${TMPDIR}/aws/install || sudo ${TMPDIR}/aws/install --update
+                else
+                     # Untar and copy/overwrite to CLIENT_LOC
+                        if [[ "$CLIENT_FILENAME" == \.gz$ && "$client" != "az" && "$client" != "aws" && -w "${CLIENT_CHECK}" ]];
+                                echo "Now unTar-ing $client client into $CLIENT_LOC" 
+                                then tar xvzf ${TMPDIR}/${CLIENT_FILENAME} -C $CLIENT_LOC --overwrite && echo "$client client installed/updated in $CLIENT_LOC"
+                        elif [[ "${client}" == "ocm" ]];
+                                        then cp ${TMPDIR}/$CLIENT_FILENAME $CLIENT_LOC/$client
+                        else
+                                sudo tar xvzf ${TMPDIR}/${CLIENT_FILENAME} -C $CLIENT_LOC --overwrite && echo "$client client installed/updated in $CLIENT_LOC with sudo permissions"
+                        fi
+                fi
 fi
 
 }
@@ -201,7 +306,7 @@ for client in "${CLIENT_ARRAY[@]}"
      	then read -p "Please input one of the following [rosa|ocm|tkn|kn|helm|oc|az]: " client	
      fi
 
-     ${ostype}_client_check_n_update
+     not_${ostype}_client_check_n_update
      # Clean up function call
      clean_up
 done
